14fbc73fce387cff4e0c7ed806f778c950b80ef4
Savannah Scott <sascott624@gmail.com>
2016-07-14 19:23:13 +0000

    updated user routes, add design index view

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root 'welcome#home', as: 'welcome'
 
   devise_for :users
-  
+
+  resources :users, only: [:show]
+  resources :designs
+  resources :stones
+
 end
