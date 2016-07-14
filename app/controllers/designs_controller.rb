34b86d81be0f5e00e457f25a376fe11bca8a03ee
Savannah Scott <sascott624@gmail.com>
2016-07-14 19:57:00 +0000

    add controller actions to designs, stones

--- app/controllers/designs_controller.rb
+++ app/controllers/designs_controller.rb
@@ -4,4 +4,28 @@ class DesignsController < ApplicationController
     @designs = Design.all
   end
 
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def edit
+
+  end
+
 end
--- app/controllers/stones_controller.rb
+++ app/controllers/stones_controller.rb
@@ -4,4 +4,27 @@ class StonesController < ApplicationController
     @stones = Stone.all
   end
 
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def edit
+
+  end
 end
