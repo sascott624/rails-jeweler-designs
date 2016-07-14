ed01c790507687837c4d2b85c004ea6a56cafd59
Savannah Scott <sascott624@gmail.com>
2016-07-14 16:43:18 +0000

    set up has_many and belongs_to relationships in models

--- app/models/stone.rb
+++ app/models/stone.rb
@@ -1,2 +1,6 @@
 class Stone < ApplicationRecord
+
+  has_many :designs
+  has_many :users, through: :designs
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :designs
+  has_many :stones, through: :designs
+  
 end
