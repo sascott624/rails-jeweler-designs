f1cb27a770345a2efd953ccaf5cfec2b052b9782
Savannah Scott <sascott624@gmail.com>
2016-07-14 23:35:35 +0000

    update design form with collection select, begin to update users form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :name, :location)
+    params.require(:user).permit(:email, :password, :name, :location, designs_attributes: [:metal])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :users, only: [:show]
+  resources :users
   resources :designs
   resources :stones
 
