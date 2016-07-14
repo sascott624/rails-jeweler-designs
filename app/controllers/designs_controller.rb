0d49bdcc581b2aa33fc153d9130c89afa2dfcce2
Savannah Scott <sascott624@gmail.com>
2016-07-14 20:01:52 +0000

    flushed out controller actions for designs, stones

--- app/controllers/designs_controller.rb
+++ app/controllers/designs_controller.rb
@@ -5,27 +5,39 @@ class DesignsController < ApplicationController
   end
 
   def create
-
+    @design = Design.new(design_params)
+    if @design.save
+      redirect_to design_path(@design)
+    else
+      render :new
+    end
   end
 
   def new
-
+    @design = Design.new
   end
 
   def show
-
+    @design = Design.find(params[:id])
   end
 
   def update
-
+    @design.update(design_params)
+    if @design.save
+      redirect_to design_path(@design)
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @design = Design.find(params[:id])
+    @design.destroy
+    redirect_to designs_path
   end
 
   def edit
-
+    @design = Design.find(params[:id])
   end
 
 end
--- app/controllers/stones_controller.rb
+++ app/controllers/stones_controller.rb
@@ -5,26 +5,38 @@ class StonesController < ApplicationController
   end
 
   def create
-
+    @stone = Stone.new(stone_params)
+    if @stone.save
+      redirect_to stone_path(@stone)
+    else
+      render :new
+    end
   end
 
   def new
-
+    @stone = Stone.new
   end
 
   def show
-
+    @stone = Stone.find(params[:id])
   end
 
   def update
-
+    @stone.update(stone_params)
+    if @stone.save
+      redirect_to stone_path(@stone)
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @stone = Stone.find(params[:id])
+    @stone.destroy
+    redirect_to stones_path
   end
 
   def edit
-
+    @stone = Stone.find(params[:id])
   end
 end
