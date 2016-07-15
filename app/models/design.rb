8a97fe2992b8f696ad398d086f5ab472d9dfb176
Savannah Scott <sascott624@gmail.com>
2016-07-15 13:58:10 +0000

    set up design instance methods

--- app/models/design.rb
+++ app/models/design.rb
@@ -6,7 +6,40 @@ class Design < ApplicationRecord
   validates :model, presence: true
 
   def price
-    # TODO - update based on design.metal and design.model
+    (self.metal_cost * self.model_cost) * self.stone.cost
   end
+
+  def metal_cost
+    case self.metal
+    when "Yellow Gold"
+      2.0
+    when "White Gold"
+      2.2
+    when "Pink Gold"
+      2
+    when "Sterling Silver"
+      1.2
+    when "Platinum"
+      3
+    else
+      nil
+    end
+  end
+
+  def model_cost
+    case self.model
+    when "Necklace"
+      2.6
+    when "Bracelet"
+      1.7
+    when "Earrings"
+      2
+    when "Ring"
+      1.2
+    else
+      nil
+    end
+  end
+
 
 end
