608398284786cd874047c0427366c10c6268f6b8
Savannah Scott <sascott624@gmail.com>
2016-07-28 16:19:23 +0000

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 stones = Stone.create([{name: "Diamond", cost: 620, weight: 0.3}, {name: "Pink Diamond", cost: 2500, weight: 1.2}, {name: "Opal", cost: 860, weight: 0.4},
-  {name: "Ruby", cost: 325, weight: .25}, {name: "Tourmaline", cost: 600, weight: 0.5}, {name: "Sapphire", cost: 1500, weight: 0.95}])
+  {name: "Ruby", cost: 325, weight: 0.25}, {name: "Tourmaline", cost: 600, weight: 0.5}, {name: "Sapphire", cost: 1500, weight: 0.95}])
 
 users = User.create([{email: "johnny@jewelry.com", password: "diamonds4ever", name: "John", location: "Timbuktu"}, {email: "carter@jeweler.com", password: "panther123", name: "Carter", location: "Greenwich, CT"},
   {email: "betty@jeweler.com", password: "cufflinks456"}])
