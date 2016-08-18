7c61d40e71e7eae1a64fb54893efe2f008b3d3cf
Savannah Scott <sascott624@gmail.com>
2016-08-18 10:58:19 -0400

    created stone function to create JS stone object

--- app/assets/javascripts/new_stone.js
+++ app/assets/javascripts/new_stone.js
@@ -11,12 +11,27 @@ var submitForm = function(){
     submitting.done(function(data){
       // here, instead of rendering JSON, create a JS stone object wtih a
       // prototype method that renders the stone.name, etc. in the page
-      $('#show').show();
-      $('#stoneDetails').text("Stone Details");
-      $('#stoneName').html("Name: " + data["name"]);
-      $('#stoneWeight').html("Weight: " + data["weight"] + " ct");
-      $('#stoneCost').html("Cost: $" + data["cost"]);
-      $('#linkToStone').html("<a href='/stones/" + data["id"] + "'>View in Index</a>");
+      var newStone = new Stone(data["name"], data["weight"], data["cost"], data["id"], data["designs"], data["users"]);
+      newStone.createHTML();
+    //   $('#show').show();
+    //   $('#stoneDetails').text("Stone Details");
+    //   $('#stoneName').html("Name: " + data["name"]);
+    //   $('#stoneWeight').html("Weight: " + data["weight"] + " ct");
+    //   $('#stoneCost').html("Cost: $" + data["cost"]);
+    //   $('#linkToStone').html("<a href='/stones/" + data["id"] + "'>View in Index</a>");
     })
   })
+};
+
+function Stone(name, weight, cost, id, designs, users){
+  this.name = name;
+  this.weight = weight;
+  this.cost = cost;
+  this.id = id;
+  this.designs = [];
+  this.users = [];
 }
+
+Stone.prototype.createHTML = function(){
+  console.log("this will be the prototype function")
+  }
