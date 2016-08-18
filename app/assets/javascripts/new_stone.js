ea5400646042811c27bf49722a2f743a6aaa1ec8
Savannah Scott <sascott624@gmail.com>
2016-08-18 11:03:54 -0400

    finalized stone prototype to render attributes in HTML

--- app/assets/javascripts/new_stone.js
+++ app/assets/javascripts/new_stone.js
@@ -9,16 +9,10 @@ var submitForm = function(){
     var values = $(this).serialize();
     var submitting = $.post('/stones', values);
     submitting.done(function(data){
-      // here, instead of rendering JSON, create a JS stone object wtih a
-      // prototype method that renders the stone.name, etc. in the page
       var newStone = new Stone(data["name"], data["weight"], data["cost"], data["id"], data["designs"], data["users"]);
+      $('#show').show();
+      $('#stoneDetails').text("Stone Details");
       newStone.createHTML();
-    //   $('#show').show();
-    //   $('#stoneDetails').text("Stone Details");
-    //   $('#stoneName').html("Name: " + data["name"]);
-    //   $('#stoneWeight').html("Weight: " + data["weight"] + " ct");
-    //   $('#stoneCost').html("Cost: $" + data["cost"]);
-    //   $('#linkToStone').html("<a href='/stones/" + data["id"] + "'>View in Index</a>");
     })
   })
 };
@@ -33,5 +27,8 @@ function Stone(name, weight, cost, id, designs, users){
 }
 
 Stone.prototype.createHTML = function(){
-  console.log("this will be the prototype function")
+    $('#stoneName').text("Name: " + this.name);
+    $('#stoneWeight').text("Weight: " + this.weight + " ct");
+    $('#stoneCost').text("Cost: $" + this.cost);
+    $('#linkToStone').html("<a href='/stones/" + this.id + "'>View in Index</a>");
   }
